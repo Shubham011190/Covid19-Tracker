b22ec5f793f348ed00fb2d7d24146fadcc5acc3b
Shubham Singhal <shubham.011190@gmail.com>
2020-04-20 18:04:59 +0530

    Cards.css added

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Cards.module.css'
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
 import CountUp from 'react-countup'
+import cx from 'classnames'
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     
@@ -12,7 +13,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify='center'>
-                <Grid item component={Card}>
+                <Grid item component={Card} className ={cx(styles.card,styles.infected)} xs={12} md={3}>    {/*xs = extrasmall, takes all 12 spaces on small devices, md= medium, taking 3 spaces out of 12 */}
                     <CardContent >
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant='h5'>
@@ -22,7 +23,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography variant='body2'>Number of active cases for COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                 <Grid item component={Card}>
+                 <Grid item component={Card} className ={cx(styles.card,styles.recovered)} xs={12} md={3}>
                     <CardContent >
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant='h5'>
@@ -32,7 +33,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography variant='body2'>Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                 <Grid item component={Card}>
+                 <Grid item component={Card} className ={cx(styles.card,styles.deaths)} xs={12} md={3}>
                     <CardContent >
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant='h5'>
